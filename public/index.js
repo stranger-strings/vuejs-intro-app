9f9e9ae1c15da5c9ab2ee18ed4a628e2f9636971
Peter Jang <peter.x.jang@gmail.com>
2017-12-14 19:57:09 -0600

    Add Vue.js frontend with v-model, v-for, and v-on:click examples

---
+++ public/index.js
@@ -0,0 +1,39 @@
+/* global Vue, VueRouter, axios */
+
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!!!!!",
+      reviews: [
+        { text: "This is cool!", rating: 5, reviewer: "Burton Wax" },
+        { text: "This is pretty okay!", rating: 3, reviewer: "Emily Post" },
+        { text: "This is pretty bad!", rating: 1, reviewer: "Angela Pearson" }
+      ],
+      newReviewText: "",
+      newReviewRating: "",
+      newReviewReviewer: ""
+    };
+  },
+  mounted: function() {},
+  methods: {
+    addReview: function() {
+      var newReview = {
+        text: this.newReviewText,
+        rating: this.newReviewRating,
+        reviewer: this.newReviewReviewer
+      };
+      this.reviews.push(newReview);
+    }
+  },
+  computed: {}
+};
+
+var router = new VueRouter({
+  routes: [{ path: "/", component: HomePage }]
+});
+
+var app = new Vue({
+  el: "#app",
+  router: router
+});
